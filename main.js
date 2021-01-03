105b3bba851377cc7caa6b264002092c95b5c565
TakemuraNaho <75403403+TakemuraNaho@users.noreply.github.com>
2021-01-03 20:42:14 +0900

    Update main.js

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-let storyText = 'ある日:insertx:はお菓子が食べたくなりました。 そして:inserty:を食べました。その後 :insertz:も食べました。花子は:insertx:の体重を聞いて絶叫しました。:insertx:の体重が300Kgになっている！';
+let storyText = 'ある日:insertx:はお菓子が食べたくなりました。 そして:inserty:を食べました。その後 :insertz:も食べました。花子は体重を聞いて絶叫しました。:insertx:の体重が300Kgになっている！';
 let insertX = ['太郎','母','父'];
 let insertY = ['ポテトチップス100枚','チョコレート1年分','揚げドーナツ30個'];
 let insertZ = ['カツ丼10kg','チーズたっぷりピザ20枚','唐揚げ100個'];
